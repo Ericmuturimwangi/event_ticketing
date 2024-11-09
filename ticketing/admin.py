02425b9ac6cdbbc26deead1c137a9edbeca9d9c5
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-11-09 11:03:45 +0300

    adding the search feature for admin panel

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Event, Ticket, Order
 
-# Register your models here.
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('event', 'type', 'price', 'available_quantity')
+    search_fields = ['event__title', 'type']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display =('user', 'event', 'ticket', 'quantity', 'total_price', 'status')
+    list_filter = ['status', 'event']
+    search_fields =['user__username', 'event__title']
+
+admin.site.register(Event)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Order, OrderAdmin)
+
