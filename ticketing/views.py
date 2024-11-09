9b985e203b0c6c708cd8334af4ee33906afd4c87
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-11-09 10:43:34 +0300

    added the viewsets for handling crud operations to the project

--- ticketing/views.py
+++ ticketing/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from ticketing.serializers import EventSerializer, TicketSerializer, OrderSerializer
+from .models import Event, Ticket, Order
+from rest_framework import viewsets
 
-# Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    
